94ba47d769c1c68728de145f184fc050e9625de3
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-01-29 15:37:20 +0900

    daisyuiのインストール

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Page() {
-  return <h1 className="text-3xl font-bold underline bg-amber-400">Hello, Next.js!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline bg-amber-400">Hello, Next.js!</h1>
+      <button className="btn">Button</button>
+    </div>
+  );
 }
