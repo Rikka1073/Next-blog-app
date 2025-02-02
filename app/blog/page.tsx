5bfeff7088cc75b322d0b1553a60c553e47c2970
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-02 21:43:31 +0900

    blogの内容設定

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+"use client";
+import DOMPurify from "dompurify";
+import { useEffect, useState } from "react";
 import { client } from "../libs/client";
 
-const page = async () => {
-  const data = await client.get({
-    endpoint: "blogs",
-    contentId: "0dfhd7c78li",
-  });
+const page = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const fetchCms = async () => {
+      const data = await client.get({
+        endpoint: "blogs",
+      });
+      setBlogs(data.contents);
+    };
+    fetchCms();
+  }, []);
+
+  const sanitizeText = (html: string) => {
+    return DOMPurify.sanitize(html, { ALLOWED_TAGS: [] });
+  };
 
   return (
     <div>
-      <div>{data.title}</div>
+      {blogs.map((blog) => {
+        return (
+          <div key={blog.id}>
+            <div>{blog.title}</div>
+            <div>{sanitizeText(blog.content)}</div>
+          </div>
+        );
+      })}
+      <div>おはよう</div>
     </div>
   );
 };
