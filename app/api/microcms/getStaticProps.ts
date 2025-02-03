7eceb2837c4464ad1f90b60dd796ec19d9542b22
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-03 21:27:35 +0900

    Apiのエンドポイント修正

--- app/api/microcms/getStaticProps.ts
+++ app/api/microcms/getStaticProps.ts
@@ -2,7 +2,7 @@ import { client } from "../../libs/client";
 
 export const getStaticProps = async () => {
   const data = await client.get({
-    endpoint: "hello",
+    endpoint: "blogs",
   });
 
   return {
