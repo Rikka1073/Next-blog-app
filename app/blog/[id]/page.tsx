af4883f580a855487abced7e37e07765c2148d4a
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-06 18:05:12 +0900

    Header挿入

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import DOMPurify from "dompurify";
 import BlogLayout from "../../../components/blog/BlogLayout";
 import Image from "next/image";
+import BlogHeader from "../../../components/blog/BlogHeader";
 
 const page = () => {
   const [blogsData, setBlogsData] = useState([]);
@@ -38,21 +39,30 @@ const page = () => {
   };
 
   return (
-    <BlogLayout>
-      {blogsData.map((data) => {
-        return (
-          <div className="max-w-3xl mx-auto" key={data.id}>
-            <h2 className="text-4xl mb-12">Blogs</h2>
-            <div className="mb-4">{formatDate(data.createdAt)}</div>
-            <h3 className="text-2xl mb-4">{data.title}</h3>
-            <div className="mb-8">
-              <Image src={data.eyecatch.url} width={600} height={200} alt="" className="mx-auto" />
+    <>
+      <BlogHeader />
+      <BlogLayout>
+        {blogsData.map((data) => {
+          return (
+            <div className="max-w-3xl mx-auto" key={data.id}>
+              <h2 className="text-4xl mb-12">Blogs</h2>
+              <div className="mb-4">{formatDate(data.createdAt)}</div>
+              <h3 className="text-2xl mb-4">{data.title}</h3>
+              <div className="mb-8">
+                <Image
+                  src={data.eyecatch.url}
+                  width={600}
+                  height={200}
+                  alt=""
+                  className="mx-auto"
+                />
+              </div>
+              <div className="leading-10">{sanitizeText(data.content)}</div>
             </div>
-            <div className="leading-10">{sanitizeText(data.content)}</div>
-          </div>
-        );
-      })}
-    </BlogLayout>
+          );
+        })}
+      </BlogLayout>
+    </>
   );
 };
 
--- components/blog/BlogHeader.tsx
+++ components/blog/BlogHeader.tsx
@@ -3,10 +3,16 @@ import React from "react";
 
 const BlogHeader = () => {
   return (
-    <header>
-      <div>
-        <Link href="/blog">Blog</Link>
-        <Link href="/">TOP</Link>
+    <header className="w-full border-b-2 border-gray-300  py-6">
+      <div className="max-w-3xl mx-auto ">
+        <div className="flex gap-10 text-xl">
+          <Link href="/" className="hover:text-blue-400">
+            TOP
+          </Link>
+          <Link href="/blog" className="hover:text-blue-400">
+            Blog
+          </Link>
+        </div>
       </div>
     </header>
   );
