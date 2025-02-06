1fdcce45a7d9f776bd722142392ea336d84078d0
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-06 18:09:48 +0900

    headerだしわけ

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,9 +4,12 @@ import { useEffect, useState } from "react";
 import { client } from "../libs/client";
 import Link from "next/link";
 import BlogLayout from "../../components/blog/BlogLayout";
+import { usePathname } from "next/navigation";
+import BlogHeader from "../../components/blog/BlogHeader";
 
 const page = () => {
   const [blogs, setBlogs] = useState([]);
+  const url = usePathname();
 
   useEffect(() => {
     const fetchCms = async () => {
@@ -21,23 +24,26 @@ const page = () => {
   }, []);
 
   return (
-    <BlogLayout>
-      <div className="flex flex-wrap gap-4">
-        {blogs &&
-          blogs.map((blog) => {
-            return (
-              <div className="card bg-base-100 w-96 shadow-xl" key={blog.id}>
-                <Link href={`/blog/${blog.id}`}>
-                  <div className="card-body">
-                    <div>{blog.title}</div>
-                    <div>{blog.date}</div>
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
-      </div>
-    </BlogLayout>
+    <>
+      <BlogHeader url={url} />
+      <BlogLayout>
+        <div className="flex flex-wrap gap-4">
+          {blogs &&
+            blogs.map((blog) => {
+              return (
+                <div className="card bg-base-100 w-96 shadow-xl" key={blog.id}>
+                  <Link href={`/blog/${blog.id}`}>
+                    <div className="card-body">
+                      <div>{blog.title}</div>
+                      <div>{blog.date}</div>
+                    </div>
+                  </Link>
+                </div>
+              );
+            })}
+        </div>
+      </BlogLayout>
+    </>
   );
 };
 
--- components/blog/BlogHeader.tsx
+++ components/blog/BlogHeader.tsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
 import React from "react";
 
-const BlogHeader = () => {
+const BlogHeader = ({ url }) => {
   return (
     <header className="w-full border-b-2 border-gray-300  py-6">
       <div className="max-w-3xl mx-auto ">
         <div className="flex gap-10 text-xl">
-          <Link href="/" className="hover:text-blue-400">
+          <Link href="/" className="hover:text-gray-400">
             TOP
           </Link>
-          <Link href="/blog" className="hover:text-blue-400">
+          <Link
+            href="/blog"
+            className={`hover:text-gray-400 ${url === "/blog" ? "hidden" : "block"}`}
+          >
             Blog
           </Link>
         </div>
