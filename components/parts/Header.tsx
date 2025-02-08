b48fdcd5637fe050a7a21c13d837b3f0c0751ab3
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2025-02-08 15:06:25 +0900

    header遷移先修正

--- components/parts/Header.tsx
+++ components/parts/Header.tsx
@@ -6,8 +6,8 @@ type HeaderProps = {
 };
 const Header = ({ url }: HeaderProps) => {
   return (
-    <header className="w-full border-b-2 border-gray-300  py-6">
-      <div className="max-w-3xl mx-auto ">
+    <header className="w-full border-b-2 border-gray-300 py-6 mb-15">
+      <div className="max-w-3xl mx-auto">
         <div className="flex gap-10 text-xl">
           <Link href="/" className="hover:text-gray-400">
             TOP
@@ -20,7 +20,7 @@ const Header = ({ url }: HeaderProps) => {
           </Link>
           <Link
             href="/blogs"
-            className={`hover:text-gray-400 ${url === "/blog" ? "hidden" : "block"}`}
+            className={`hover:text-gray-400 ${url === "/blogs" ? "hidden" : "block"}`}
           >
             Blog
           </Link>
